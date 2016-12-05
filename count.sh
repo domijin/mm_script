a2f324dfaec1072f35b984c31db7416f677178e8
Dongming Jin <contact@domij.info>
2016-12-05 00:03:32 -0600

    update work dir to scratch

--- count.sh
+++ count.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-RES="/work/04159/domij/res"
-wc -l $RES/done.list
-echo `ls $RES/bak/* | wc -l` "$RES/bak  tar file"
-echo `ls $RES/alpha/*.dat | wc -l` "$RES/alpha lag.dat"
-echo `ls -d $RES/alpha/model* | wc -l` "$RES/alpha models"
-echo $((`ls $RES/alpha/model*/* | wc -l`/3)) "RES/alpha/model* mm files"
-echo $((`ls $RES/BHsystem/* | wc -l`/3)) "RES/BHsystem/* BH system files"
+WORK="/scratch/04159/domij"
+wc -l $WORK/res/done.list
+echo `ls $WORK/res/bak/* | wc -l` "$WORK/res/bak  tar file"
+echo `ls $WORK/res/alpha/*.dat | wc -l` "$WORK/res/alpha lag.dat"
+echo `ls -d $WORK/res/alpha/model* | wc -l` "$WORK/res/alpha models"
+echo `ls $WORK/res/alpha/model*/* | wc -l` "$WORK/res/alpha/model* mm files"
+echo `ls $WORK/res/BHsystem/* | wc -l` "$WORK/res/BHsystem/* BH system files"
