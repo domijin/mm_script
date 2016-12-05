3a0c106eaabec59eea88133e85b4326cff30c26a
Dongming Jin <contact@domij.info>
2016-12-05 12:05:56 -0600

    debug only the exact model

--- debug.sh
+++ debug.sh
@@ -2,11 +2,11 @@
 model=$1
 WORK="/scratch/04159/domij"
 
-trials=`grep $model $(ls $WORK/param/paramlist* | grep -v all) | wc -l`
-ofile=`grep $model $WORK/stdout/*.o | tail -n 1 | cut -d ':' -f 1 | rev | cut -d '/' -f 1 | rev`
+trials=`grep \ $model $(ls $WORK/param/paramlist* | grep -v all) | wc -l`
+ofile=`grep \ $model $WORK/stdout/*.o | tail -n 1 | cut -d ':' -f 1 | rev | cut -d '/' -f 1 | rev`  # stdout/   output file for job
 
-list=`grep $model $(ls $WORK/param/paramlist* | grep -v all)`
-pfile=`echo $list | cut -d ':' -f 1 | rev | cut -d '/' -f 1 | rev`
+list=`grep \ $model $(ls $WORK/param/paramlist* | grep -v all)`
+pfile=`echo $list | cut -d ':' -f 1 | rev | cut -d '/' -f 1 | rev` # paramlist file
 
 echo 'submitted' $trials 'times, in' $pfile 
 echo 'last process output is' $ofile 
