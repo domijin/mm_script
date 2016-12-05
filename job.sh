3e49fcf2037a89220391fde0985438fa4a537700
Dongming Jin <contact@domij.info>
2016-12-04 23:28:49 -0600

    modify the path in job.sh for unity

--- job.sh
+++ job.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-RES="/work/04159/domij/res"
-WORK="/scratch/04159/domij/Models"
+WORK="/scratch/04159/domij"
 model=$1
 jid=$2
 
-if [ ! -d $RES ]; then
-mkdir $RES $RES/alpha $RES/bak $RES/BHsystem
+if [ ! -d $WORK/res ]; then
+mkdir $WORK/res $WORK/res/alpha $WORK/res/bak $WORK/res/BHsystem
 fi
 
-if [ ! -d $WORK/model-$model ]; then
+if [ ! -d $WORK/Models/model-$model ]; then
     echo "warning: model" $model "does not exist" 
 else
-    cd $WORK/model-$model
+    cd $WORK/Models/model-$model
     echo model-$model $jid
     pwd 
 
@@ -25,23 +24,23 @@ else
 	if [[ `tail -n 1 *-output.dat | awk '{print $1}'` == "restart.fil"  ]]; then
 
 # ejected BH system
-	    awk '$14==14 || $29==14 {printf("%2.4f Gyrs %2d %2d %5.4f M_sol %5.4f M_sol %2.4f AU %.4f %d %d\n",$7/1000.0,$14,$29,$19,$34,$44,$45,$4,$5)}' escape_binary.dat > $RES/BHsystem/$model-bhe.dat
+	    awk '$14==14 || $29==14 {printf("%2.4f Gyrs %2d %2d %5.4f M_sol %5.4f M_sol %2.4f AU %.4f %d %d\n",$7/1000.0,$14,$29,$19,$34,$44,$45,$4,$5)}' escape_binary.dat > $WORK/res/BHsystem/$model-bhe.dat
 
 # retained BH system
-	    awk '$4=="###" {TIME=$2} TIME>12000 && TIME<12050 && ($8==14 || $9==14) {printf("%2.4f Gyrs %2d %2d %5.4f M_sol %5.4f M_sol %2.4f AU %.4f %d %d %d\n",TIME/1000.0,$8,$9,$10,$11,$29,$21,$1,$5,$6)}' snapshot.dat > $RES/BHsystem/$model-bhr.dat
+	    awk '$4=="###" {TIME=$2} TIME>12000 && TIME<12050 && ($8==14 || $9==14) {printf("%2.4f Gyrs %2d %2d %5.4f M_sol %5.4f M_sol %2.4f AU %.4f %d %d %d\n",TIME/1000.0,$8,$9,$10,$11,$29,$21,$1,$5,$6)}' snapshot.dat > $WORK/res/BHsystem/$model-bhr.dat
 
 # ejected single BH
-	    awk '$8>100 {printf("%2.4f Gyrs %2d %5.4f M_sol %d %d %d\n",$2/1000.0,$6,$8,7,$4,$5)}' escape.dat > $RES/BHsystem/$model-sbhe.dat
+	    awk '$8>100 {printf("%2.4f Gyrs %2d %5.4f M_sol %d %d %d\n",$2/1000.0,$6,$8,7,$4,$5)}' escape.dat > $WORK/res/BHsystem/$model-sbhe.dat
 
 # lag
-	    cp lagrangi.dat $RES/alpha/$model-lag.dat
+	    cp lagrangi.dat $WORK/res/alpha/$model-lag.dat
 
 # for mm alpha
-	    mkdir $RES/alpha/model-$model
-	    cp mocca.ini end.dat system.dat $RES/alpha/model-$model/
+	    mkdir $WORK/res/alpha/model-$model
+	    cp mocca.ini end.dat system.dat $WORK/res/alpha/model-$model/
 
 # tar & clean up
-	    tar -czf $RES/bak/$model.tar.gz snapshot.dat mocca* escape* lagrangi* system.dat profile.dat 
+	    tar -czf $WORK/res/bak/$model.tar.gz snapshot.dat mocca* escape* lagrangi* system.dat profile.dat 
 
 	    cd ..
 	
@@ -51,10 +50,10 @@ else
 
 	    rm -r model-$model
 	    dt=`date`
-	    echo model-$model $jid 'runtime:' $t $dt >> $RES/done.list
+	    echo model-$model $jid 'runtime:' $t $dt >> $WORK/res/done.list
 	fi
     else
 	dt=`date`
-	echo model-$model $dt >> $RES/unfinished.list
+	echo model-$model $dt >> $WORK/res/unfinished.list
     fi
 fi
