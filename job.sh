73e56dc72a3b9ce6192b28f862a4237d82145e85
Domi Jin <contact@domij.info>
2016-12-16 15:14:20 -0600

    sbhe.dat  instead of 7

--- job.sh
+++ job.sh
@@ -27,10 +27,10 @@ else
 	    awk '$14==14 || $29==14 {printf("%2.4f Gyrs %2d %2d %5.4f M_sol %5.4f M_sol %2.4f AU %.4f %d %d\n",$7/1000.0,$14,$29,$19,$34,$44,$45,$4,$5)}' escape_binary.dat > $w_dir/res/BHsystem/$model-bhe.dat
 
 # retained BH system
-	    awk '$4=="###" {TIME=$2} TIME>12000 && TIME<12050 && ($8==14 || $9==14) {printf("%2.4f Gyrs %2d %2d %5.4f M_sol %5.4f M_sol %2.4f AU %.4f %d %d %d\n",TIME/1000.0,$8,$9,$10,$11,$29,$21,$1,$5,$6)}' snapshot.dat > $w_dir/res/BHsystem/$model-bhr.dat
+	    awk '$4=="###" {TIME=$2} TIME>12000 && TIME<12050 && ($8==14 || $9==14) {printf("%2.4f Gyrs %2d %2d %5.4f M_sol %5.4f M_sol %2.4f AU %.4f %d %d %d\n",TIME/1000.0,$8,$9,$10,$11,$20,$21,$1,$5,$6)}' snapshot.dat > $w_dir/res/BHsystem/$model-bhr.dat
 
 # ejected single BH
-	    awk '$8>100 {printf("%2.4f Gyrs %2d %5.4f M_sol %d %d %d\n",$2/1000.0,$6,$8,7,$4,$5)}' escape.dat > $w_dir/res/BHsystem/$model-sbhe.dat
+	    awk '$8>100 {printf("%2.4f Gyrs %2d %5.4f M_sol %d %d %d\n",$2/1000.0,$6,$8,$7,$4,$5)}' escape.dat > $w_dir/res/BHsystem/$model-sbhe.dat
 
 # lag
 	    cp lagrangi.dat $w_dir/res/alpha/$model-lag.dat
